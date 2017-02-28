4771ddef045e775b82f25bf3f21d5feefe52140e
hoeksejr <joshhoeksema@gmail.com>
2017-02-28 18:58:30 -0500

    Update TestHello.java
    Fixing test case

--- TestHello.java
+++ TestHello.java
@@ -5,6 +5,6 @@ public class TestHello extends TestCase {
     public void testHello()
     {
         Hello h = new Hello();
-        assertTrue(h.greet(), "Hello, world");
+        assertEquals(h.greet(), "Hello, world");
     }
 }
